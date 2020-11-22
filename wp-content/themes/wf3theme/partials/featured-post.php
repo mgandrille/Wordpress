e55eb77b949d9e2696e90dd9d9c0136fa1044b3f
Marie Gandrille <mgandrille@yahoo.fr>
2020-11-22 18:12:20 +0100

    Nouvelle mise en forme de la page d'accueil

--- wp-content/themes/wf3theme/partials/featured-post.php
+++ wp-content/themes/wf3theme/partials/featured-post.php
@@ -6,20 +6,27 @@ $args=array('posts_per_page'=>1, 'tag' => 'en-avant');
 $wp_query = new WP_Query( $args );
 ?>
 
-<?php if ( have_posts() ) :
-    while (have_posts()) : the_post(); ?>
-<div class="jumbotron p-4 p-md-5 text-white rounded bg-dark">
-    <div class="col-md-6 px-0">
-        <h1 class="display-4 font-italic"><?php the_title(); ?></h1>
-        <p class="lead my-3"><?php the_excerpt(); ?></p>
-        <p class="lead mb-0"><a href="<?php echo get_permalink(); ?>" class="text-white font-weight-bold">Continue
-                reading...</a></p>
-    </div>
+<div class="container">
+    <?php if ( have_posts() ) :
+        while (have_posts()) : the_post(); ?>
+            <div class="jumbotron row p-4 p-md-5 rounded bg-light">
+                <div class="col-md-6 px-0">
+                    <h1 class="display-4 font-italic"><?php the_title(); ?></h1>
+                    <p class="lead my-3"><?php the_excerpt(); ?></p>
+                    <p class="lead mb-0">
+                        <a href="<?php echo get_permalink(); ?>" class="font-weight-bold">
+                            Continue reading...
+                        </a>
+                    </p>
+                </div>
+                <div class="col-md-5 offset-1 d-none d-lg-block">
+                    <img src="<?php the_post_thumbnail_url() ?>" alt="" class="bd-placeholder-img" width=100% height=100%>
+                </div>
+            </div>
+        <?php endwhile;
+    endif; ?>
 </div>
-<?php endwhile;
-endif; ?>
 
-
-<?php 
+<?php
 $wp_query = $original_query;
 wp_reset_postdata(); ?>
